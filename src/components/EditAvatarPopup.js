72c9db848ed74a8d4b1588803c0736ca796b2ec1
kheir93 <70963707+kheir93@users.noreply.github.com>
2022-05-21 19:04:36 +0200

    project-14_review_itteration_2

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,17 +1,7 @@
 import React, {useRef, useState} from "react";
 import PopupWithForm from "./PopupWithForm";
 
-export default function EditAvatarPopup({isOpen, onClose, onOverlayClick, onUpdateAvatar}) {
-  const [isValid, setIsValid] = useState(false);
-
-  const inputError = document.querySelector(".form__input-error")
-
-  // function handleAvatarField() {
-
-  //   if (inputError != inputError.src) {
-  //     return inputError.classList.add("form__input_type_error")
-  //   }
-  // }
+export default function EditAvatarPopup({ isOpen, onClose, onOverlayClick, onUpdateAvatar }) {
 
   const avatarRef = useRef()
 
@@ -20,15 +10,14 @@ export default function EditAvatarPopup({isOpen, onClose, onOverlayClick, onUpda
     e.preventDefault();
     //Value to pass to the external handler//
     onUpdateAvatar({
-    avatar: avatarRef.current.value
-  });
-}
+      avatar: avatarRef.current.value
+  });}
 
   return (
-    <PopupWithForm onSubmit={handleSubmit} isOpen={isOpen} onClose={onClose} onOverlayClick={onOverlayClick} name="avatar" title="Change profile picture" buttonName="Save" submitValue="Submit">
+    <PopupWithForm onSubmit={handleSubmit}  isOpen={isOpen} onClose={onClose} onOverlayClick={onOverlayClick} name="avatar" title="Change profile picture" buttonName="Save" submitValue="Submit" >
       <label className="form__field">
-        <input type="url" className="form__input form__input_avatar" ref={avatarRef} placeholder="Avatar image link" defaultValue="" name="avatar" required/>
-        <span className="form__input-error">Please enter a web address.</span>
+        <input type="url" className="form__input form__input_avatar" placeholder="Avatar image link" defaultValue="" name="avatarURL" id="inputAvatar" ref={ avatarRef } required/>
+        <span className={'form__input-error inputAvatar-error'}>Please enter a web address.</span>
       </label>
     </PopupWithForm>
   );
--- src/components/SignForm.js
+++ src/components/SignForm.js
@@ -8,17 +8,14 @@ const SignForm = ({ title, onChangePassword, onChangeEmail, authMode, authAction
       <h2 className="auth-modal__title">{title}</h2>
       <label className="auth-modal__field">
         <input type="email" id="email" className="auth-modal__input auth-modal__input_email" name="email" placeholder="Email" onChange={onChangeEmail} value={email} />
-        <span className="auth-modal__input-error inputEmail-error">Please fill out this field.</span>
       </label>
       <label className="auth-modal__field">
         <input type="password" id="password" className="auth-modal__input auth-modal__input_password" name="password" placeholder="Password" onChange={onChangePassword} value={password} />
-        <span className="auth-modal__input-error inputPassword-error">Please fill out this field.</span>
       </label>
       <button className="auth-modal__submit">{title}</button>
       <p className="auth-modal__status">{authMode}
         <Link to={link} className="auth-modal__status-link">{authAction}</Link>
       </p>
-
     </form>
   )
 }
